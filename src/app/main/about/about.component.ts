d4f91c0af5238b0a2dd15b9327c445cc7908f81b
liz0921 <lmonteror@ucenfotec.ac.cr>
2023-08-05 17:04:28 -0600

    Botón de descarga de cv

--- src/app/main/about/about.component.ts
+++ src/app/main/about/about.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
-
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent {
+  descargarPDF() {
+    fetch('assets/CV-Lizbeth-Montero.pdf')
+      .then((response) => response.blob())
+      .then((blob) => {
+        const url = URL.createObjectURL(blob);
 
+        // Crear un enlace temporal
+        const link: HTMLAnchorElement = document.createElement('a');
+        link.href = url;
+        link.download = 'CV-Lizbeth-Montero.pdf'; // Nombre del archivo al descargarlo
+
+        // Simular un clic en el enlace para descargar el archivo
+        link.click();
+
+        // Eliminar el enlace temporal
+        URL.revokeObjectURL(url);
+      });
+  }
 }
+
+
+
+
+
+
+
+
+
+
+  
