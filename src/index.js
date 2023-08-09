3ebd1ec7d46a7a7df60a056b116c1ac55abe9650
liz0921 <lmonteror@ucenfotec.ac.cr>
2023-08-09 00:13:06 -0600

    version 1.27

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
 
-const port = 3000;
-const address = '0.0.0.0';
-
-app.listen(port, address, () => {
-  console.log(`App is listening at ${address}:${port}`);
+const port = process.env.PORT || 3000; // Obtener el puerto de una variable de entorno o usar el puerto 3000 por defecto
+app.get("/");
+app.listen(port, '0.0.0.0', () => {
+  console.log(`App is listening at 0.0.0.0:${port}`);
 });
