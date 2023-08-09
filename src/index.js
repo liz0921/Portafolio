e01cbfb88b7ad066a231c63018d29ef01413c515
liz0921 <lmonteror@ucenfotec.ac.cr>
2023-08-09 08:54:08 -0600

    version 1.229

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-const port = process.env.PORT || 3000; // Obtener el puerto de una variable de entorno o usar el puerto 3000 por defecto
-app.listen(port, '0.0.0.0', () => {
-  console.log(`App is listening at 0.0.0.0:${port}`);
+// Define la carpeta de distribución de la aplicación Angular
+const distFolder = path.join(__dirname, 'dist', 'Portafolio'); // Reemplaza 'nombre-de-tu-app' con el nombre real de tu aplicación
+
+// Configura el middleware para servir los archivos estáticos
+app.use(express.static(distFolder));
+
+// Configura una ruta para manejar todas las demás solicitudes y redirigirlas a la página principal
+app.get('/', (req, res) => {
+  res.sendFile(path.join(distFolder, 'index.html'));
+});
+
+// Inicia el servidor en el puerto deseado
+const port = process.env.PORT || 3000; // Obtén el puerto de una variable de entorno o usa el puerto 3000 por defecto
+app.listen(port, () => {
+  console.log(`Servidor en ejecución en el puerto ${port}`);
 });
